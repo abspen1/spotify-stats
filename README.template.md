261ed26f49a8fe7cc6726bc766b3a98eecabef8f
abspen1 <abspencer2097@gmail.com>
2021-01-12 21:38:58 -0700

    big update

---
+++ README.template.md
@@ -0,0 +1,13 @@
+# Last.fm pulling data from Spotify
+
+This is an auto updating readme test repository
+
+## Most recent song
+
+![Song Cover Photo]({img})
+**{song}
+**{artist}
+
+## Works Cited
+
+- [Source](https://dev.to/gargakshit/how-i-added-my-spotify-statistics-to-my-github-readme-4jdd)
--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-var Spotify = require('spotify-web-api-js');
-var s = new Spotify();
-//s.searchTracks()...
+require("isomorphic-unfetch");
+var Redis = require('ioredis')
+const { promises: fs } = require("fs");
+const path = require("path");
+
+const redisPass = process.env.REDIS_PASS;
+const redisHost = process.env.REDIS_HOST;
+
+var redis = new Redis({
+    port: 6379,          // Redis port
+    host: redisHost,   	 // Redis host
+    password: redisPass, // Redis pass
+    db: 11,				 // Redis database
+});
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+    ).toString("utf-8");
+    
+    const name = await redis.hget('current-track', 'Name')
+    const artist = await redis.hget('current-track', 'Artist')
+    const img = await redis.hget('current-track', 'Image')
+
+
+    const readme = readmeTemplate
+        .replace("{song}", name)
+        .replace("{artist}", artist)
+        .replace("{img}", img);
+
+    await fs.writeFile("README.md", readme);
+}
+
+main();
